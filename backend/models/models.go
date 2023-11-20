02dbf932dff8bcc0da4502f9928f74c02b2324c4
HarshKakran <harshkakran05@gmail.com>
2023-11-20 11:37:01 +0530

    Added ID to the models

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 type User struct {
+	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Role     string `json:"role"`
 	Username string `json:"username"`
@@ -8,14 +9,19 @@ type User struct {
 }
 
 type Store struct {
+	// ID    int    `json:"id"`
 	Owner User   `json:"owner"`
 	Type  string `json:"type"`
 	Items []Item `json:"items"`
 }
 
 type Item struct {
+	ID          int    `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Type        string `json:"type"`
 	Quantity    int    `json:"quantity"`
 }
+
+// TODO: Will be useful when using JWT or any other type of authentication
+// var UserStoreMapping = make(map[User]Store)
