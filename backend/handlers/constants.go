814bc2a16645457b0b38843d5a7dabc84c908a14
HarshKakran <harshkakran05@gmail.com>
2023-11-20 12:29:06 +0530

    created a constants.go for storing all the constants

---
+++ backend/handlers/constants.go
@@ -0,0 +1,11 @@
+package handlers
+
+var USER_ROLE_OWNER = "owner"
+var USER_ROLE_CONSUMER = "consumer"
+
+var STORE_TYPE_FRUITS_AND_VEGETABLE = "fruits&vegetable"
+var STORE_TYPE_KIRYANA = "kiryana"
+
+var ITEM_TYPE_FRUITS = "fruits"
+var ITEM_TYPE_VEGETABLE = "vegetable"
+var ITEM_TYPE_GROCERIES = "groceries"
