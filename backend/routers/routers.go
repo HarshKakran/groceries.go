75fafa74cd900fee2c397465df2a9b7442f5db71
HarshKakran <harshkakran05@gmail.com>
2023-11-20 11:42:25 +0530

    Created routes

--- backend/routers/routers.go
+++ backend/routers/routers.go
@@ -1,1 +1,25 @@
 package routers
+
+import (
+	"github.com/HarshKakran/groceries.go/handlers"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	// common routes
+	router.HandleFunc("/api/signin", handlers.Signin).Methods("POST")
+	router.HandleFunc("/api/signup", handlers.Signup).Methods("POST")
+	router.HandleFunc("/api/signout", handlers.Signout).Methods("GET")
+
+	// owner routes
+	router.HandleFunc("/api/store/", handlers.StoreInfo).Methods("GET")
+	router.HandleFunc("/api/store/item", handlers.CreateItem).Methods("POST")
+	router.HandleFunc("api/store/item/{id}", handlers.UpdateItem).Methods("PUT")
+	router.HandleFunc("api/store/item/{id}", handlers.DeleteItem).Methods("DELETE")
+
+	// consumer routes
+	router.HandleFunc("api/consumer", handlers.ConsumerPage).Methods("GET")
+	router.HandleFunc("api/consumer/checkout", handlers.CreateOrder).Methods("POST")
+}
