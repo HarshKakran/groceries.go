27c18126b80f9bb7dc0ab71c2bf96b7ea1ea6aca
HarshKakran <harshkakran05@gmail.com>
2023-11-20 12:23:41 +0530

    Added the code to start the server

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/HarshKakran/groceries.go/routers"
+)
+
+func main() {
+	r := routers.Router()
+	port := ":8080"
+
+	// Start the server
+	go func() {
+		err := r.Run(port)
+		if err != nil {
+			fmt.Printf("Error starting server: %v\n", err)
+		}
+	}()
+
+	fmt.Printf("Server is now running on port %s\n", port)
+	fmt.Println("Starting GO API service...")
+	fmt.Println(`
+ ______     ______        ______     ______   __    
+/\  ___\   /\  __ \      /\  __ \   /\  == \ /\ \   
+\ \ \__ \  \ \ \/\ \     \ \  __ \  \ \  _-/ \ \ \  
+ \ \_____\  \ \_____\     \ \_\ \_\  \ \_\    \ \_\ 
+  \/_____/   \/_____/      \/_/\/_/   \/_/     \/_/ `)
+
+	// This is to keep the main goroutine from exiting immediately
+	select {}
 }
