30e71ab8c227801cf0dd3646d6b5e64bf79a34b2
HarshKakran <harshkakran05@gmail.com>
2023-11-20 14:45:18 +0530

    Created the CRUD APIs for the OWNER

--- backend/handlers/owner_handlers.go
+++ backend/handlers/owner_handlers.go
@@ -14,9 +14,10 @@ func StoreInfo(c *gin.Context) {
 	if err != nil {
 		log.Fatalf("error while converting string to int. %v", err)
 	}
-	for _, store := range models.Stores {
-		if store.Owner.ID == userId {
-			c.JSON(http.StatusFound, store)
+	for _, user := range models.Users {
+		if user.ID == userId && user.Store != nil {
+			c.JSON(http.StatusFound, *user.Store)
+			return
 		}
 	}
 	c.JSON(http.StatusNotFound, gin.H{
@@ -24,6 +25,101 @@ func StoreInfo(c *gin.Context) {
 	})
 }
 
-func CreateItem(c *gin.Context) {}
-func UpdateItem(c *gin.Context) {}
-func DeleteItem(c *gin.Context) {}
+func CreateItem(c *gin.Context) {
+	userId, err := strconv.Atoi(c.GetHeader(HEADER_USER_ID))
+	if err != nil {
+		log.Fatalf("error while converting string to int. %v", err)
+	}
+
+	// extracting request body
+	var item models.Item
+	if err := c.ShouldBindJSON(&item); err != nil {
+		log.Panicf("error while extracting item data. %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	for _, user := range models.Users {
+		if user.ID == userId && user.Store != nil {
+			user.Store.Items = append(user.Store.Items, item)
+			c.JSON(http.StatusOK, gin.H{"message": "item added"})
+			return
+		}
+	}
+	c.JSON(http.StatusBadRequest, gin.H{
+		"message": "store not found",
+	})
+}
+
+func UpdateItem(c *gin.Context) {
+	userId, err := strconv.Atoi(c.GetHeader(HEADER_USER_ID))
+	if err != nil {
+		log.Fatalf("error while converting string to int. %v", err)
+	}
+
+	itemId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalf("error while converting string to int. %v", err)
+	}
+
+	for _, user := range models.Users {
+		if user.ID == userId && user.Store != nil {
+			for index, item := range user.Store.Items {
+				if index == itemId {
+					if err := c.ShouldBindJSON(&item); err != nil {
+						log.Panicf("error while extracting item data. %v", err)
+						c.JSON(http.StatusBadRequest, gin.H{
+							"message": err.Error(),
+						})
+						return
+					}
+					user.Store.Items[index] = item
+					c.JSON(http.StatusOK, gin.H{
+						"message": "item added",
+						"store":   user.Store,
+					})
+					return
+				}
+			}
+		}
+	}
+	c.JSON(http.StatusBadRequest, gin.H{
+		"message": "item id not found",
+	})
+
+}
+
+func DeleteItem(c *gin.Context) {
+	userId, err := strconv.Atoi(c.GetHeader(HEADER_USER_ID))
+	if err != nil {
+		log.Fatalf("error while converting string to int. %v", err)
+	}
+
+	itemId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalf("error while converting string to int. %v", err)
+	}
+
+	for _, user := range models.Users {
+		if user.ID == userId && user.Store != nil {
+			for index, _ := range user.Store.Items {
+				if index == itemId {
+					user.Store.Items = append(user.Store.Items[:index], user.Store.Items[index+1:]...)
+					c.JSON(http.StatusOK, gin.H{
+						"message": "item deleted",
+					})
+					return
+				}
+			}
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "item not found",
+			})
+		}
+	}
+
+	c.JSON(http.StatusBadRequest, gin.H{
+		"message": "store not found",
+	})
+}
